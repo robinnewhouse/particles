7d9cb9ae4ff17fb23a0235afdb35418c41d88f60
Robin Newhouse <robinnewhouse@gmail.com>
2020-08-30 23:16:28 +0200

    Making a window

--- main.cpp
+++ main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
+
 #define SDL_MAIN_HANDLED
+
 #include "SDL.h"
+
 using namespace std;
 //#undef main
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
 
-//  SDL_SetMainReady();
-  if (SDL_Init(SDL_INIT_VIDEO < 0)){
+  const int SCREEN_WIDTH = 800;
+  const int SCREEN_HEIGHT = 600;
+
+  if (SDL_Init(SDL_INIT_VIDEO < 0)) {
     cout << "SDL_Init Failed" << endl;
-    return 6;
+    return 1;
+  };
+
+  SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+  if (window == nullptr) {
+    cout << "Could not create window" << endl;
+    return 2;
   };
   
+  _sleep(1000);
+  
   cout << "Success" << endl;
-
+  SDL_DestroyWindow(window);
   SDL_Quit();
 
   return 0;
