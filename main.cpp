307e46e2186380cfa11e07600a41411f9b662bf6
Robin Newhouse <robinnewhouse@gmail.com>
2020-08-31 22:32:29 +0200

    Can exit out of a window using SDL_QUIT

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #define SDL_MAIN_HANDLED
 
 #include "SDL.h"
+#include "windows.h"
 
 using namespace std;
 //#undef main
@@ -18,16 +19,30 @@ int main() {
     return 1;
   };
 
-  SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  SDL_Window *window = SDL_CreateWindow("Particle Fire Explosion", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                        SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
   if (window == nullptr) {
     cout << "Could not create window" << endl;
     return 2;
   };
-  
-  _sleep(1000);
-  
-  cout << "Success" << endl;
+
+  bool quit = false;
+  while (!quit) {
+    // Update particles
+    // Draw particles
+    // Check for messages/events
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        quit = true;
+      }
+    }
+  }
+//  Sleep(5000);
+
+  cout << "Destroying windows" << endl;
   SDL_DestroyWindow(window);
   SDL_Quit();
 
