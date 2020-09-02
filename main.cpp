157fb13462905dcabed57614b439bf9d55954804
Robin Newhouse <robinnewhouse@gmail.com>
2020-09-02 13:49:40 +0200

    Using memset to set a solid window color

--- main.cpp
+++ main.cpp
@@ -27,6 +27,33 @@ int main() {
     return 2;
   };
 
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+  SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STATIC, SCREEN_WIDTH, SCREEN_HEIGHT); // try SDL_TEXTUREACCESS_STREAMING here
+
+  if (renderer == nullptr) {
+    cout << "Could not create renderer" << endl;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 3;
+  }
+  if (texture == nullptr) {
+    cout << "Could not create texture" << endl;
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 4;
+  }
+
+  Uint32 *buffer = new Uint32[SCREEN_WIDTH * SCREEN_HEIGHT];
+//  Uint32 * buffer2 = new Uint32[SCREEN_WIDTH*SCREEN_HEIGHT];
+  memset(buffer, 0xFF, SCREEN_WIDTH * SCREEN_HEIGHT * sizeof(Uint32));
+
+  SDL_UpdateTexture(texture, NULL, buffer, SCREEN_WIDTH * sizeof(Uint32));
+  SDL_RenderClear(renderer);
+  SDL_RenderCopy(renderer, texture, NULL, NULL);
+  SDL_RenderPresent(renderer);
+
+
   bool quit = false;
   while (!quit) {
     // Update particles
@@ -43,6 +70,8 @@ int main() {
 //  Sleep(5000);
 
   cout << "Destroying windows" << endl;
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyTexture(texture);
   SDL_DestroyWindow(window);
   SDL_Quit();
 
